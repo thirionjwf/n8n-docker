d05ae01941592140f29c8ba17dd2988a1375319f
Dérik Thirion <thirionjwf@gmail.com>
2026-04-25 23:51:16 +0200

    Implement silence console beep configuration
    Add systemd service and Xsession script to silence console beep

--- debian-bell.sh
+++ debian-bell.sh
@@ -2,3 +2,28 @@ gsettings set org.gnome.desktop.wm.preferences audible-bell false
 gsettings set org.gnome.desktop.wm.preferences visual-bell true
 gsettings set org.gnome.desktop.wm.preferences visual-bell-type frame-flash
 
+sudo su -
+cat > /etc/systemd/system/silence-console.service <<EOD
+[Unit]
+Description=Silence virtual console default beep
+
+[Service]
+Type=oneshot
+Environment=TERM=linux
+StandardOutput=tty
+TTYPath=/dev/console
+ExecStart=/usr/bin/setterm -blength 0
+
+[Install]
+WantedBy=multi-user.target 
+EOD
+
+systemctl daemon-reload
+systemctl enable silence-console
+systemctl start silence-console
+
+cat > /etc/X11/Xsession.d/91custom-silence-beep <EOD
+#!/bin/sh
+xset -b
+EOD
+chmod +x /etc/X11/Xsession.d/91custom-silence-beep
