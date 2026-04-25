c02eaf6e885213658f10c54c5d36a095858c532d
Dérik Thirion <thirionjwf@gmail.com>
2026-04-25 23:43:00 +0200

    Disable bell in terminal

---
+++ debian-bell.sh
@@ -0,0 +1,4 @@
+gsettings set org.gnome.desktop.wm.preferences audible-bell false
+gsettings set org.gnome.desktop.wm.preferences visual-bell true
+gsettings set org.gnome.desktop.wm.preferences visual-bell-type frame-flash
+
